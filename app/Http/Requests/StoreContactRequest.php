58437f8a999af602fe2a6f1123fbeb3cfb0b556d
Yenner_lnx <yenner.211999@gmail.com>
2022-07-23 18:36:55 -0500

    Create Form Request to validate store form

---
+++ app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'phone_number' => 'required|numeric',
+            'email' => 'required|email',
+            'age' => 'nullable|numeric',
+            'profile_picture' => 'nullable|image',
+        ];
+    }
+}
